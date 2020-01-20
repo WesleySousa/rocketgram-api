367d1f4e7560398828e47a158a956e943d575ec8
Wesley Silva <wesley@simplecode.eti.br>
2020-01-19 21:35:36 -0300

    atualizado versao do mongoose

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://semana:semana@cluster0-fbgem.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 app.use((req, res, next) => {
